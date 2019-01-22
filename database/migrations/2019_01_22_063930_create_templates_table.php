b568257ba7eb41abea82267ab634d2c86f5489a2
Andrei Kunda <ogniemimechem@gmail.com>
2019-01-22 11:14:41 +0300

    add css/js files. template migration

---
+++ database/migrations/2019_01_22_063930_create_templates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            //заголовок варчар 255
+            $table->string('title',255);
+            $table->text('text');
+            //краткое описание
+            $table->text('price');
+            //псевдоним/ уникальное
+            $table->string('alias',150)->unique();
+            $table->string('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+}
