c625f4829436d1d08ee69927687d881298a8d6f3
Andrei Kunda <ogniemimechem@gmail.com>
2019-02-05 22:43:41 +0300

    edit index template

--- resources/views/site/footer_top.blade.php
+++ resources/views/site/footer_top.blade.php
@@ -1,53 +1,37 @@
-<div class="zigzag-bottom"></div>
+
 <div class="container">
     <div class="row">
         <div class="col-md-3 col-sm-6">
             <div class="footer-about-us">
-                <h2>u<span>Stora</span></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
-                <div class="footer-social">
-                    <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                </div>
+                <h2><span>YourTemplate</span></h2>
+                <p>Большое разнообразие шаблонов на любой вкус.</p>
             </div>
         </div>
 
         <div class="col-md-3 col-sm-6">
             <div class="footer-menu">
-                <h2 class="footer-wid-title">User Navigation </h2>
+                <h2 class="footer-wid-title">Навигация пользователя </h2>
                 <ul>
-                    <li><a href="#">My account</a></li>
-                    <li><a href="#">Order history</a></li>
-                    <li><a href="#">Wishlist</a></li>
-                    <li><a href="#">Vendor contact</a></li>
-                    <li><a href="#">Front page</a></li>
+                    @if(  Auth::user() )
+                        <li><a href="{{ route('user_panel') }}"><i class="fa fa-user"></i> Мой аккаунт</a></li>
+                    @else
+                        <li><a href="{{ route('login') }}"><i class="fa fa-user"></i> Войти</a></li>
+                        <li><a href="{{ route('register') }}"><i class="fa fa-user"></i> Регистрация</a></li>
+                    @endif
+                    <li><a href="{{ route('cart') }}"><i class="fa fa-user"></i> Моя корзина</a></li>
                 </ul>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-6">
-            <div class="footer-menu">
-                <h2 class="footer-wid-title">Categories</h2>
-                <ul>
-                    <li><a href="#">Mobile Phone</a></li>
-                    <li><a href="#">Home accesseries</a></li>
-                    <li><a href="#">LED TV</a></li>
-                    <li><a href="#">Computer</a></li>
-                    <li><a href="#">Gadets</a></li>
-                </ul>
-            </div>
-        </div>
 
         <div class="col-md-3 col-sm-6">
             <div class="footer-newsletter">
-                <h2 class="footer-wid-title">Newsletter</h2>
-                <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                <h2 class="footer-wid-title">Подпишись</h2>
+                <p>Подпишись на нашу рассылку и получай уникальные предложения</p>
                 <div class="newsletter-form">
                     <form action="#">
-                        <input type="email" placeholder="Type your email">
-                        <input type="submit" value="Subscribe">
+                        <input type="email" placeholder="Введите Ваш email">
+                        <input type="submit" value="Подписаться">
                     </form>
                 </div>
             </div>
